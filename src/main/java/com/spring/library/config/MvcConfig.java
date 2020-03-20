25baf1a27657ee757e8b22713e5f64e8da7840db
Александр <medvedev.sasha2000@mail.ru>
2020-03-20 14:15:33 +0300

    Fixed settings path

--- src/main/java/com/spring/library/config/MvcConfig.java
+++ src/main/java/com/spring/library/config/MvcConfig.java
@@ -25,7 +25,7 @@ public class MvcConfig implements WebMvcConfigurer {
     }
 
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/login").setViewName("log/login");
         registry.addViewController("/").setViewName("greeting");
     }
 }
--- src/main/java/com/spring/library/controller/SettingsController.java
+++ src/main/java/com/spring/library/controller/SettingsController.java
@@ -8,37 +8,36 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
 @Controller
-@RequestMapping("/settings")
 public class SettingsController {
 
     @Autowired
     private SettingsService settingsService;
 
 
-    @GetMapping
+    @GetMapping("/settings")
     public String getUserSettingsPage(@AuthenticationPrincipal User user, Model model) {
         model.addAttribute("username", user.getUsername());
-        return "settings";
+        return "user/userSettings";
     }
 
-    @PostMapping
-    public String updateUserSettings(@AuthenticationPrincipal User user,
-                                     @RequestParam("currentPasswordConfirmation") String currentPasswordConfirmation,
-                                     @RequestParam("newPassword") String newPassword,
-                                     @RequestParam("newPasswordConfirmation") String newPasswordConfirmation,
-                                     Model model
+    @PostMapping("/settings")
+    public String updateUserSettings(
+            @AuthenticationPrincipal User user,
+            @RequestParam("currentPasswordConfirmation") String currentPasswordConfirmation,
+            @RequestParam("newPassword") String newPassword,
+            @RequestParam("newPasswordConfirmation") String newPasswordConfirmation,
+            Model model
     ) {
         Map<String, Object> passwordErrorMap = settingsService.checkErrorsInPasswords(user, currentPasswordConfirmation,
                 newPassword, newPasswordConfirmation);
         if (passwordErrorMap.isEmpty()) {
             settingsService.updateUserSettings(user, newPassword);
-            return  "redirect:/";
+            return "redirect:/";
 //            return "redirect:/logout";
         }
 
