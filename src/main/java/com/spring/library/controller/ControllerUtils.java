ae12824c6a82dd08751d75ee2d4ce064309a45da
Александр <medvedev.sasha2000@mail.ru>
2020-03-21 21:13:58 +0300

    Add NPE checks

--- src/main/java/com/spring/library/controller/ControllerUtils.java
+++ src/main/java/com/spring/library/controller/ControllerUtils.java
@@ -1,8 +1,15 @@
 package com.spring.library.controller;
 
+import com.spring.library.domain.Book;
+import com.spring.library.domain.Review;
+import com.spring.library.domain.User;
+import com.spring.library.domain.Writer;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
@@ -14,6 +21,30 @@ class ControllerUtils {
                 FieldError::getDefaultMessage
         );
         return bindingResult.getFieldErrors().stream().collect(collector);
+    }
+
+    static void isBookExists(Book book) {
+        if (book == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "BOOK NOT FOUND");
+        }
+    }
+
+    static void isWriterExists(Writer writer) {
+        if (writer == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "WRITER NOT FOUND");
+        }
+    }
+
+    static void isUserProfileExists(User userProfile) {
+        if (userProfile == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "USER NOT FOUND");
+        }
+    }
+
+    static void isReviewExists(Review review) {
+        if (review == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "REVIEW NOT FOUND");
+        }
     }
 
 }
